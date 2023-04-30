02502f0a36907915b0b018c29baa125d72b1ebc9
Julio <jueg25@gmail.com>
2023-04-29 22:18:55 -0500

    overhaul to new discord interaction api thing

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# mutebot
+# MuteBot
+
+This is a simple Discord bot built with Node.js and the Discord.js library. The bot responds to a `/mute` command by muting the specified user for one minute, then unmuting them. It also keeps track of muted users in a database.
+
+## Usage
+
+To use the bot, type `/mute @username` in any channel. The bot will mute the specified user for one minute, then unmute them.
--- bot.js
+++ bot.js
@@ -1,85 +1,137 @@
-const Discord = require('discord.js');
+const { token } = require('./config.json');
+const { Client, Intents, MessageEmbed } = require('discord.js');
+
 const Keyv = require('keyv');
-const token = '' ;
-const prefix = '';
-var log_channel = 000000000000000000;
-const client = new Discord.Client();
 db = new Keyv();
 
-db.on('error', err => console.log('Database Connection Error', err));
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_MEMBERS,
+  ],
+});
 
-// role persistence event
+async function MutedRole(guild) {
+  try {
+    const mutedRole = await guild.roles.create({
+      name: 'Muted',
+      color: '#010101',
+      permissions: [],
+    });
+
+    guild.channels.cache.forEach(async (channel, id) => {
+      try {
+        if (channel.type === 'GUILD_TEXT') {
+          await channel.permissionOverwrites.edit(mutedRole, {
+            SEND_MESSAGES: false,
+            ADD_REACTIONS: false
+          });
+        } else if (channel.type === 'GUILD_VOICE'){
+          await channel.permissionOverwrites.edit(mutedRole, {
+            CONNECT: false
+          });
+        }
+      } catch (err) {
+        console.error(`Error setting permissions for channel ${channel.name}:`, err);
+      }
+    });
+
+    return mutedRole;
+  } catch (err) {
+    console.error('Error creating muted role:', err);
+  }
+}
+
+async function muteUser(interaction) {
+  const guild = interaction.guild;
+  const user = interaction.options.getUser('user');
+  const member = guild.members.cache.get(user.id);
+
+  // Check if user is a bot
+  if (user.bot) {
+    return interaction.reply("You can't mute a bot.");
+  }
+
+  // Check if the Muted role exists, and create it if not
+  const mutedRole = guild.roles.cache.find(role => role.name === 'Muted') ?? await MutedRole(guild);
+
+  // Check if the user is already muted
+  if (member.roles.cache.has(mutedRole.id)) {
+    await interaction.followUp(`${user.tag} is already muted.`);
+    return;
+  }
+
+  // Create the poll embed message
+  const embed = new MessageEmbed()
+    .setColor('#00ff00')
+    .setTitle(`Vote to mute ${user.username}#${user.discriminator}`)
+    .setDescription(`Do you agree to mute this user?\n\nThis poll will last for 1 minute.`)
+    .setThumbnail(user.avatarURL({ size: 128, dynamic: true }))
+    .setFooter({ 'text': `Poll started by ${interaction.user.username}#${interaction.user.discriminator}` })
+    .setTimestamp()
+
+  // Send the poll embed message and add reactions
+  const pollMessage = await interaction.reply({ embeds: [embed], fetchReply: true });
+  await pollMessage.react('👍');
+  await pollMessage.react('👎');
+
+  // Create a reaction collector to tally votes
+  const filter = (reaction, user) => ['👍', '👎'].includes(reaction.emoji.name) && !user.bot;
+  const collector = pollMessage.createReactionCollector({ filter, time: 60000 });
+  let yesVotes = 0;
+  let noVotes = 0;
+
+  // Listen for reactions and tally the votes
+  collector.on('collect', (reaction, user) => {
+    if (reaction.emoji.name === '👍') {
+      yesVotes++;
+    } else if (reaction.emoji.name === '👎') {
+      noVotes++;
+    }
+  });
+
+  // After the poll ends, determine the outcome and act accordingly
+  collector.on('end', async () => {
+    if (yesVotes > noVotes) {
+      // Mute the user
+      await member.roles.add(mutedRole.id);
+      await db.set(`${guild.id}_${user.id}`);
+      console.log(`Muted user ${user.id} in guild ${guild.id} at ${new Date().toISOString()}`);
+      await interaction.followUp(`User ${user.username}#${user.discriminator} has been muted.`);
+      setTimeout(async () => {
+        await member.roles.remove(mutedRole.id);
+        await db.delete(`${guild.id}_${user.id}`);
+      }, 900000); // Remove the mute role and delete the TTL after 15 minutes
+    } else {
+      await interaction.followUp(`Poll to mute user ${user.username}#${user.discriminator} failed.`);
+    }
+  });
+}
+
 client.on('guildMemberAdd', async (member) => {
-    try { //try to execute this
-        const inmate = await db.get(member.id) //search the member that joined in the outlaw database
-        const i = 1
-        if(inmate){ //and if they are in there
-            const role = await member.guild.roles.cache.find(r => r.name === 'Muted'); //find role
-            await member.roles.add(role) //mute the fucker again
-            x = i + await db.get(member.id);
-            if (x >= 4) {
-                const channel = client.channels.cache.find(ch => ch.id == log_channel);
-                channel.send('<@!' + member.id + '>' + ' is being a very bothersome user, they have rejoined to avoid mute a total of ' + x + ' times now.');
-            }
-            await db.delete(member.id); 
-            await db.set(member.id, x, 900000 * x ) //add the muted into the database, with a longer mute
-            setTimeout(() => {  msg.mentions.members.first().roles.remove(role);  }, 900000 * x); //wait 15 minutes, remove role
-        }
-    } catch (e) { //but if it fails...do this
-        console.log(e);
-        const channel = client.channels.cache.find(ch => ch.id == log_channel);
-        return channel.send('I seem to have failed, I need help.');
+  // Check if the user had been previously muted
+  const muted = await db.get(`${member.guild.id}_${member.id}`);
+  if (muted) {
+    // Assign the Muted role to the user
+    const mutedRole = member.guild.roles.cache.find(role => role.name === 'Muted');
+    try {
+      await member.roles.add(mutedRole);
+    } catch (error) {
+      console.error(`Error assigning Muted role to user ${member.user.tag}:`, error);
     }
+  }
 });
 
-client.on('message', async (msg) => {
-    try { //try to execute this
-        if (!msg.content.startsWith(prefix)) return; //returns if message didn't start with the defined prefix
-        if(msg.author.equals(client.user)) return; //returns if bot tries to mute themselves
-        
-        const role = await msg.guild.roles.cache.find(r => r.name === 'Muted'); //find role
-        if(!role) return msg.channel.send('No role was found, please make sure you have a Muted role!'); //make sure there is a role
-        
-        if(!msg.mentions.users.first()) return msg.channel.send('You need to mention somebody!'); //check if no user was mentioned
-        if(msg.mentions.has(client.user)) return msg.channel.send('Go mute yourself, bitch.'); //return message if user was trying to mute this bot
-        if(msg.mentions.members.first().roles.cache.has(role.id)) return msg.channel.send('User is already muted.'); //return message if user already has mute role
-        
-        const voting = new Discord.MessageEmbed() //generate voting embed
-        .setColor('#42b34d')
-        .setTitle('Vote to mute this fucker!')
-        .setDescription('Do you want to mute this bastard?')
-        .setFooter('Mute ' + msg.mentions.users.first().tag + ' for 15m?')
-        .setImage(msg.mentions.users.first().displayAvatarURL());
-        const sentEmbed = await msg.channel.send(voting); //send embed
-        
-        const agree = '✅'; //define agree emoji
-        const disagree = '❌'; //define disagree emoji
-        await sentEmbed.react(agree); //react with agree emoji
-        await sentEmbed.react(disagree); //react with disagree emoji
-        const filter = (reaction, user) => (reaction.emoji.name === agree || reaction.emoji.name === disagree) && !user.bot; //filter for reactions
-        const voteStatus = await msg.channel.send('@everyone' + ' Voting has started, 1 minute left...'); //start message
-        const collected = await sentEmbed.awaitReactions(filter, { time: 60000 }); //start collecting reactions
-        const agreed = collected.get(agree) || { count: 1 }; //retrieve reactions
-        const disagreed = collected.get(disagree) || { count : 1 }; //retrieve reactions
-        const agreed_count = agreed.count - 1 ; //count away bot votes
-        const disagreed_count = disagreed.count - 1; //count away bot votes
-        voteStatus.edit('Voting ended with: ' + agreed_count + agree + ' and ' + disagreed_count + disagree); //edit message to show outcome
-        if (agreed.count > disagreed.count && agreed.count >= 4) {
-            await msg.mentions.members.first().roles.add(role); //add the muted role to said user
-            await db.set(msg.mentions.users.first().id, 1, 900000) //add the muted's id into the database, expires in 15 minutes
-            setTimeout(() => {  msg.mentions.members.first().roles.remove(role);  }, 900000); //wait 15 minutes, remove role
-        }
-        else {
-            msg.channel.send('Mute voting failed :)'); //oops, voting failed :(
-        }
-    } catch (e) { //but if it fails...do this
-        return msg.channel.send('I seem to have failed, please contact the administration team.');
-    }
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand() || interaction.commandName !== 'mute') return;
+  await muteUser(interaction);
 });
 
-client.on('ready', () => {
-    console.log ('Dziala');
-    client.user.setActivity('weak-minded individuals.', { type: 'WATCHING' })
+client.once('ready', async () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity('weak-minded individuals.', { type: 'WATCHING' })
 });
 
 client.login(token);
